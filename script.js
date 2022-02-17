091716acf4201446aa817a6db3ca5fd867ce5322
奥村仁翔 <>
2022-02-17 16:56:10 +0900

    timetableの切り替えを実装しました

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function(){
+    $(".day1btn").on("click",function(){
+        $(".day1").removeClass("hide");
+        $(".day2").addClass("hide");
+        $(".day3").addClass("hide");
+    })
+    $(".day2btn").on("click",function(){
+        $(".day2").removeClass("hide");
+        $(".day1").addClass("hide");
+        $(".day3").addClass("hide");
+    })
+    $(".day3btn").on("click",function(){
+        $(".day3").removeClass("hide");
+        $(".day2").addClass("hide");
+        $(".day1").addClass("hide");
+    })
+});
