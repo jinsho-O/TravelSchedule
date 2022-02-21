9d3dc31d6ff22ca1d9a1e8359cf9ff613247f537
奥村仁翔 <>
2022-02-21 14:06:12 +0900

    軽微な修正を加えました

--- script.js
+++ script.js
@@ -93,4 +93,66 @@ $(function () {
           "fx": "fade"
         });
       });
+      let index = 0;
+  let slideMax = $('.slide').length
+  $('.right-arrow').on('click',function () {
+    if (index !== slideMax-1) {
+      index++;
+      num = index * -1 * 100;
+      $('.slide-container').css({'transform':`translateX(${num}%)`})
+    }
+  })
+  $('.left-arrow').on('click',function () {
+    if (index !== 0) {
+      index--;
+      num = index * -1 * 100;
+      $('.slide-container').css({'transform':`translateX(${num}%)`})
+    }
+  })
+
+  $(".loading").height($(window).height());
+$(".loading").width($(window).width());
+
+    
+$(".loading img").css({
+    paddingTop: ($(".loading").height() - $(".loading img").height()) / 2,
+    paddingLeft: ($(".loading").width() - $(".loading img").width()) / 2
+});
+
+$(window).resize(function () {
+   
+    "use strict";
+    
+    $(".loading").height($(window).height());
+    $(".loading").width($(window).width());
+
+
+    $(".loading img").css({
+        paddingTop: ($(".loading").height() - $(".loading img").height()) / 2,
+        paddingLeft: ($(".loading").width() - $(".loading img").width()) / 2
+    });
+    
+});
+
+$(window).mousemove(function (e) {
+   
+    "use strict";
+    
+    $(".original").css({
+        left: e.pageX - 16,
+        top: e.pageY - 16
+    });
+    
+});
+
+$("body").on("click", function (e) {
+   
+    "use strict";
+    
+    $(".original").clone(true).appendTo("body").css({
+        left: e.pageX - 16,
+        top: e.pageY - 16
+    }).removeClass("original");
+    
+});
 });
